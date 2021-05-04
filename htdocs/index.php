5fd0ce65badc7565fa9320a1ebc666d76d2530d9
Angelo Ricardo <50966170+cowboy-js@users.noreply.github.com>
2021-05-04 17:06:41 -0300

    htdocs

---
+++ htdocs/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Curso PHP</title>
+</head>
+
+<body>
+	<?php
+
+	echo 'utilizando a tag padrão';
+
+	?>
+	<br>
+	<?=
+
+	'utilizando a tag de impressão'
+	?>
+	<br>
+	<? echo 'utilizando a tag curta' ?>
+
+
+
+</body>
+
+</html>
